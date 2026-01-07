b9aa5146d9b15eac4337cd6e901a2e68294b756b
Hossein <hokmabad83@gmail.com>
2026-01-07 20:11:34 +0330

    moves

--- checkers.py
+++ checkers.py
@@ -34,4 +34,49 @@ class Board:
             for c in range(BOARD_SIZE):
                 if self.board[r][c] == EMPTY:
                     self.board[r][c] = WHITE
+    def print_board(self):
+        print("  " + " ".join(str(c) for c in range(BOARD_SIZE)))
+        for r in range(BOARD_SIZE):
+            print(r, end=" ")
+            for c in range(BOARD_SIZE):
+                p = self.board[r][c]
+                if p == -1:
+                    print(".", end=" ")
+                elif p == EMPTY:
+                    print("_", end=" ")
+                elif p == BLACK:
+                    print("b", end=" ")
+                elif p == WHITE:
+                    print("w", end=" ")
+                elif p == BLACK_KING:
+                    print("B", end=" ")
+                elif p == WHITE_KING:
+                    print("W", end=" ")
+            print()
+        print()
+
+    def copy(self):
+        new_board = Board()
+        new_board.board = copy.deepcopy(self.board)
+        return new_board
+
+    def apply_move(self, move):
+        piece = self.board[move[0][0]][move[0][1]]
+        self.board[move[0][0]][move[0][1]] = EMPTY
+        for i in range(1, len(move)):
+            r_from, c_from = move[i-1]
+            r_to, c_to = move[i]
+            # capture
+            if abs(r_to - r_from) == 2:
+                r_mid = (r_to + r_from)//2
+                c_mid = (c_to + c_from)//2
+                self.board[r_mid][c_mid] = EMPTY
+        r_end, c_end = move[-1]
+        # promote to king
+        if piece == BLACK and r_end == BOARD_SIZE-1:
+            piece = BLACK_KING
+        elif piece == WHITE and r_end == 0:
+            piece = WHITE_KING
+        self.board[r_end][c_end] = piece
+
 
